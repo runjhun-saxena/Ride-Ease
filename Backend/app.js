a0dcc2fd41c5f90e029050e24542ebe5b85fa506
Runjhun saxena <runjhunsaxena6@gmail.com>
2024-12-23 11:28:19 +0530

    captain ride acceptance page

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,8 @@ const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
@@ -17,12 +19,17 @@ app.use(cookieParser());
 
 
 
-
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+
+
 
 module.exports = app;
+
